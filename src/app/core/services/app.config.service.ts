d36673f9813883f4d11d47955f10a699297086e9
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2023-03-25 12:10:25 +0000

    Fix: theme update

--- src/app/core/services/app.config.service.ts
+++ src/app/core/services/app.config.service.ts
@@ -4,22 +4,35 @@ import { AppConfig } from '../interfaces/appconfig';
 
 @Injectable()
 export class ConfigService {
-    config: AppConfig = {
-        theme: 'lara-light-indigo',
-        dark: false,
-        inputStyle: 'outlined',
-        ripple: true,
-    };
+    config: AppConfig
 
     private configUpdate = new Subject<AppConfig>();
 
     configUpdate$ = this.configUpdate.asObservable();
 
-
+    /**
+     *
+     */
+    constructor() {
+        var tempConfig = {
+            theme: 'lara-light-indigo',
+            dark: false,
+            inputStyle: 'outlined',
+            ripple: true,
+        };
+        this.config = JSON.parse(localStorage.getItem("config"))??tempConfig
+        this.updateConfig(this.config)
+    }
 
     updateConfig(config: AppConfig) {
+        let themeElement = document.getElementById('theme-css');
         this.config = config;
         this.configUpdate.next(config);
+        localStorage.setItem("config",JSON.stringify(this.config))
+        themeElement.setAttribute(
+            'href',
+            'assets/theme/' + this.config.theme + '/theme.css'
+        );
     }
 
     getConfig() {
