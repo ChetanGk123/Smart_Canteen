d6f54ccc9d39efb78bf88399e0b3297028b46f9f
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2023-07-17 00:10:31 +0100

    .

--- src/app/modules/application/features/dashboard/dashboard.component.ts
+++ src/app/modules/application/features/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { ApiService } from 'src/app/core/services/api/api.service';
 import { MemberService } from 'src/app/core/services/MemberService/member.service';
 import { Router } from '@angular/router';
 import { ConfigService } from 'src/app/core/services/app.config.service';
+import { CounterService } from '../counters/counter.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     templateUrl: './dashboard.component.html',
@@ -11,33 +13,49 @@ export class DashboardComponent implements OnInit {
     dashboardData: any;
     consumablesList: any;
     Data: any;
-    loading:boolean = true
-    consumablesListLoading:boolean = true
+    counter_id:any;
+    loading: boolean = true;
+    consumablesListLoading: boolean = true;
+    // Private
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
     constructor(
         public configService: ConfigService,
+        public counterService: CounterService,
         public apiService: ApiService,
         public member: MemberService,
         public router: Router
     ) {}
 
     ngOnInit() {
-        this.loading = true
+        this.counterService.counterDate$
+            .pipe(takeUntil(this._unsubscribeAll))
+            .subscribe((data:any) => {
+                this.counter_id = data?.id ?? ''
+            });
+            this.loadData();
+    }
+
+    loadData() {
+        var url = '';
+        if(this.counter_id){
+            url = `/BY_COUNTER/${this.counter_id}`
+        }
+        this.loading = true;
         this.apiService
-            .getTypeRequest('dashboard_analytics')
+            .getTypeRequest(`dashboard_analytics${url}`)
             .toPromise()
             .then((result: any) => {
                 this.dashboardData = result.data;
-                this.loading = false
+                this.loading = false;
             });
 
-        this.consumablesListLoading = true
+        this.consumablesListLoading = true;
         this.apiService
             .getTypeRequest('consumables_data?meal_date=01-06-2023')
             .toPromise()
             .then((result: any) => {
-                this.consumablesList  = result.data.consumables_list;
-                this.consumablesListLoading = false
+                this.consumablesList = result.data.consumables_list;
+                this.consumablesListLoading = false;
             });
-
     }
 }
